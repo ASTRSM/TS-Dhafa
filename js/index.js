9eb4b539264b5c7cf1e7442cb7df1cf3c4725ec5
astrsm <ddefrito84@gmail.com>
2024-02-05 01:38:07 +0700

    style: better slide on mobile

--- js/index.js
+++ js/index.js
@@ -43,6 +43,8 @@ $(document).ready(function () {
     dots: true,
     infinite: true,
     speed: 300,
-    slidesToShow: 1
+    slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
   })
 })
