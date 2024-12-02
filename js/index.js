1179aeb5dc4200cded6493cd44d86364953fb92a
astrsm <ddefrito84@gmail.com>
2024-12-02 14:15:22 +0700

    feat(gtm): add cookie consent boilerplate

--- js/index.js
+++ js/index.js
@@ -54,3 +54,100 @@ $(document).ready(function () {
     autoplaySpeed: 8000,
   })
 })
+
+
+// cookie consent banner
+const dialog = document.querySelector('dialog')
+
+window.onload = (event) => {
+  const cookie = getCookie("cookie-consent")
+
+  if (cookie === null) {
+    dialog.showModal()
+  }
+};
+
+document.getElementById("grant").addEventListener("click", function() {
+  const cookie = JSON.parse(getCookie("cookie-consent"))
+  const checkboxes = document.querySelectorAll('input[type="checkbox"]')
+  dialog.showModal()
+
+  checkboxes.forEach(checkbox => {
+    const name = checkbox.getAttribute('name')
+    if (cookie[name] === "granted") {
+       checkbox.checked = true
+     }
+  })
+
+});
+
+document.getElementById("accept-all-cookies").addEventListener("click", function() {
+  grantAllConsents()
+})
+
+document.getElementById("accept-cookies").addEventListener("click", function() {
+  let cookies = {}
+  const formEl = document.querySelector('form')
+  const formData = new FormData(formEl)
+
+  for (var [key, value] of formData.entries()) {
+  cookies[key] = value === "granted" ? "granted" : "denied"
+  }
+
+  const data = {
+    ad_user_data: 'denied',
+    ad_personalization: 'denied',
+    ad_storage: 'denied',
+    analytics_storage: 'denied',
+    ...cookies,
+    functionality_storage: 'granted',
+    personalization_storage: 'granted',
+    security_storage: 'granted'
+  };
+
+  document.cookie = `cookie-consent=${encodeURIComponent(JSON.stringify(data))}; path=/; secure; samesite=strict; max-age=${60 * 60 * 24 * 30}`;
+
+  grantConsents()
+})
+
+document.getElementById("ignore-cookies").addEventListener("click", function() {
+  dialog.close()
+})
+
+const grantAllConsents = () => {
+  function gtag() { dataLayer.push(arguments); }
+
+  const grantAll = {
+    ad_user_data: 'granted',
+    ad_personalization: 'granted',
+    ad_storage: 'granted',
+    analytics_storage: 'granted',
+    functionality_storage: 'granted',
+    personalization_storage: 'granted',
+    security_storage: 'granted'
+  }
+
+  document.cookie = `cookie-consent=${encodeURIComponent(JSON.stringify(grantAll))}; path=/; secure; samesite=strict; max-age=${60 * 60 * 24 * 30}`;
+
+  gtag('consent', 'update', grantAll);
+  dialog.close()
+}
+
+const grantConsents = () => {
+  const consent = getCookie("cookie-consent")
+  function gtag() { dataLayer.push(arguments); }
+
+  gtag('consent', 'update', JSON.parse(consent))
+  dialog.close()
+}
+
+function getCookie(name) {
+  const cookies = document.cookie.split('; '); // Split into individual cookies
+  for (const cookie of cookies) {
+    const [key, value] = cookie.split('='); // Split into key and value
+    if (key === name) {
+      return decodeURIComponent(value); // Decode the cookie value
+    }
+  }
+  return null; // Return null if the cookie is not found
+}
